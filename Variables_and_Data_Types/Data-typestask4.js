5d1b4babe8245663c99a0fbc7146492a178166d0
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-07-14 12:02:25 +0530

    data types task 3

---
+++ Variables_and_Data_Types/Data-typestask4.js
@@ -0,0 +1,16 @@
+const newnum =  12;
+const fullName = 'subha';
+const data = (23 > 12);
+const isdata = {
+        name : "pradip",
+        no : 123,
+}
+const day = [ 12, 34, 56, 435]
+
+
+
+console.log(typeof newnum);
+console.log(typeof fullName);
+console.log(typeof data);
+console.log(typeof isdata);
+console.log(typeof day);
